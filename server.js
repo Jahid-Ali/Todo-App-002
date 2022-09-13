a49a671c9a8a74bfb56018e48a42bdd2cc7b60ae
Jahid Ali <66717944+Jahid-Ali@users.noreply.github.com>
2022-09-13 23:53:02 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+const PORT = process.env.PORT || 5000
+
+const app = express();
+app.use(cors());
+//use express.json() to get data into json format 
+app.use(express.json());
+// import routes
+app.use(require("./routes/todoRoutes"));
+
+
+
+// import database
+require("./db/connect");
+// import model/collection
+require("./models/todoSchema");
+
+
+
+// HEROKU 
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"))
+
+    const path = require('path')
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+app.listen(PORT, () => {
+    console.log(`server running at http://localhost:${PORT}`);
+})
